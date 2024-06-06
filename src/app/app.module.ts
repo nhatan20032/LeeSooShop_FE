213fcbffbbda5320490f02df4b5c9fedf001f761
nhatan20032 <119768785+nhatan20032@users.noreply.github.com>
2024-06-06 14:55:22 +0700

    update components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserDropdownComponent } from './components/dropdowns/user-dropdown/user-dropdown.component';
+import { FooterAdminComponent } from './components/footers/footer-admin/footer-admin.component';
+import { AdminNavbarComponent } from './components/navbars/admin-navbar/admin-navbar.component';
 
 @NgModule({
   declarations: [
+    UserDropdownComponent,
+    FooterAdminComponent,
+    AdminNavbarComponent,
     AppComponent
   ],
   imports: [
