a73cbd880bb0629434b40bc3fbe820c654908fe6
nhatan20032 <119768785+nhatan20032@users.noreply.github.com>
2024-06-10 11:07:21 +0700

    update code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './layouts/admin/admin.component';
+import { HomePageComponent } from './views/admin/home-page/home-page.component';
 
 const routes: Routes = [
   {
     path: "",
     component: AdminComponent,
+    children: [
+      { path: "home-page", component: HomePageComponent },
+      { path: "", redirectTo: "home-page", pathMatch: "full" },
+    ],
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserDropdownComponent } from './components/dropdowns/user-dropdown/user-dropdown.component';
-import { FooterAdminComponent } from './components/footers/footer-admin/footer-admin.component';
 import { AdminNavbarComponent } from './components/navbars/admin-navbar/admin-navbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { AdminComponent } from './layouts/admin/admin.component';
+import { HeaderStatsComponent } from './components/headers/header-stats/header-stats.component';
+import { CardStatsComponent } from './components/cards/card-stats/card-stats.component';
+import { CardPageVisitsComponent } from './components/cards/card-page-visits/card-page-visits.component';
+import { CardSocialTrafficComponent } from './components/cards/card-social-traffic/card-social-traffic.component';
+import { HomePageComponent } from './views/admin/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     UserDropdownComponent,
-    FooterAdminComponent,
     AdminNavbarComponent,
     SidebarComponent,
     AdminComponent,
+    HeaderStatsComponent,
+    CardStatsComponent,
+    CardPageVisitsComponent,
+    CardSocialTrafficComponent,
+    HomePageComponent,
     AppComponent
   ],
   imports: [
